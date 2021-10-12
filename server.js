78e2149e182f4b86c7b4058a36e2a95de62cb40d
aniruddha <aniruddha.k@staqwise.com>
2021-10-12 13:29:38 +0530

    [Title] Welcome
    [Description]
    
    [Cause & Countermeasure]
    [Issue#]
    
    [Developer] Aniruddha Kulkarni <anikulkarni100@gmail.com>

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 const { PORT = 3000 } = process.env
 
 app.get("/", function (req, res) {
-    res.send("It worked !!!!!!!!!!!!!!!!!!!! ")
+    res.send("Welcome !! ")
 })
 
 app.get("/me", function (req, res) {
-    res.send("/me route worked")
+    res.send("me route worked")
 })
 
 app.listen(PORT || 5000)
