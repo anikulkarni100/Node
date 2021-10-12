5b3247aa884cbe87225c5cf8293ba985a58434ca
aniruddha <aniruddha.k@staqwise.com>
2021-10-12 12:47:49 +0530

    [Title] changes
    [Description]
    
    [Cause & Countermeasure]
    [Issue#]
    
    [Developer] Aniruddha Kulkarni <anikulkarni100@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const app = express();
 
-app.get("/",function(req,res){
+const { PORT = 3000 } = process.env
+
+app.get("/", function (req, res) {
     res.send("It worked ")
 })
 
-app.get("/me",function(req,res){
+app.get("/me", function (req, res) {
     res.send("me worked")
 })
 
-app.listen(precess.env.PORT || 5000)
+app.listen(PORT || 5000)
+
 module.exports = app
